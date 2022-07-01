85294f954a787fc2fcd553dfc3a40264a1b5f765
brettjanz <brettjanz1@gmail.com>
2022-06-30 19:46:46 -0700

    Add button to BlogPageLayout to add new post, only visible to administrators

--- vega-web-master/src/components/templates/BlogPageLayout.js
+++ vega-web-master/src/components/templates/BlogPageLayout.js
@@ -1,13 +1,19 @@
-import {Container, Row} from 'react-bootstrap';
+import {Button, Container, Row} from 'react-bootstrap';
 import Header from '../UI/organisms/Header.js';
 import Footer from '../UI/organisms/Footer.js';
 import ListNewsDetails from '../UI/organisms/ListNewsDetails.js'
+import { useContext } from 'react';
+import { UserContext } from '../../auth/UserProvider.js';
 
 const BlogPageLayout = ({listOfNews}) => {
+	const { user } = useContext(UserContext);
+	const addPostButton = (user.role == "ROLE_ADMIN") ? <Button onClick={() => console.log("Add Post")}>New Post</Button> : <div></div>;
+	
 	return(
 		<Container className="d-flex flex-column min-vh-100 justify-content-between">
 			<Row>
 				<Header />
+				{addPostButton}
 				<ListNewsDetails listOfNews={listOfNews}/>
 			</Row>
 			<Footer />
