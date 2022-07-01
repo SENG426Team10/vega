680af07f0c40d013dc3f815d6f47a1b4b512372b
brettjanz <brettjanz1@gmail.com>
2022-06-30 17:41:56 -0700

    Add functionality to add secret button

--- vega-web-master/src/components/pages/Vault.js
+++ vega-web-master/src/components/pages/Vault.js
@@ -1,16 +1,80 @@
-import { useContext } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import { Row, Col, Table, Button } from 'react-bootstrap';
+import { useContext, useState } from 'react';
+import { Row, Col, Table, Button, Form, Modal } from 'react-bootstrap';
 import SimplePageLayout from '../templates/SimplePageLayout.js';
 import { UserContext } from '../../auth/UserProvider.js';
 
-
 const Vault = (props) => {
 	const { user, setUserInfo, logout } = useContext(UserContext);
+	const [show, setShow] = useState(false);
+	const [name, setName] = useState('');
+	const [secret, setSecret] = useState(null);
+	const [listOfSecrets, setSecrets] = useState([]);
 
-	const addSecret = () => {
-		console.log("Added");
+	const addSecret = (name_, secret_) => {
+		setSecrets(prevList => [...prevList, {
+			name: name_,
+			date: new Date().toLocaleDateString(),
+			secret: secret_
+		}]);
+
+		console.log("Added " + name_ + ":" + secret_);
+	}
+
+	const addSecretForm = () => {		
+
+		const nameChangeHandler = (event) => {
+			setName(event.target.value);
+        }
+
+		const secretChangeHandler = (event) => {
+			setSecret(event.target.value);
+		}
+
+		const submitHandler = (event) => {
+			event.preventDefault();
+
+			addSecret(name, secret);
+			setShow(false)
+        }
+
+		return (
+			<Form onSubmit={submitHandler}>
+				<Form.Group>
+					<Form.Label>Secret Name</Form.Label>
+					<Form.Control type="text" onChange={nameChangeHandler} placeholder="Enter a name for your secret" />
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Secret</Form.Label>
+					<Form.Control type="text" onChange={secretChangeHandler}/>
+				</Form.Group>
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		);
     }
+		
+	const addButton = () => {
+		const handleClose = () => setShow(false);
+		const handleShow = () => setShow(true);
+
+		return (
+			<>
+				<Button onClick={handleShow}>
+					Add Secret
+				</Button>
+
+				<Modal show={show} onHide={handleClose}>
+					<Modal.Header closeButton>
+						<Modal.Title>Add New Secret</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						{addSecretForm()}
+					</Modal.Body>
+				</Modal>
+			</>
+		);
+	}
 
 	const shareSecret = () => {
 		console.log("Shared");
@@ -22,28 +86,21 @@ const Vault = (props) => {
 
 	const deleteSecret = () => {
 		console.log("Deleted");
-    }
+	}
 
-	const addButton = <Button onClick={addSecret} size="sm">Add Secret</Button>
 	const shareButton = <Button onClick={shareSecret} size="sm">Share</Button>;
 	const updateButton = <Button onClick={updateSecret} size="sm">Update</Button>;
 	const deleteButton = <Button onClick={deleteSecret} size="sm">Delete</Button>;
 
 	const listOfSecretsHTML = () => {
-		return (
-			<tr>
-				<td>Birth Certificate</td><td>07/10/1999</td><td>Dummy Data</td><td>{shareButton}</td><td>{updateButton}</td><td>{deleteButton}</td>
-			</tr>
-		);
+		if (listOfSecrets.length) {
+			return listOfSecrets.map((record) => <tr><td>{record.name}</td><td>{record.date}</td><td>{record.secret}</td><td>{shareButton}</td><td>{updateButton}</td><td>{deleteButton}</td></tr>)
+		}
     }
 
 	return (
 		<SimplePageLayout>
 			<Row>
-				<Col sm={6}>
-					<p>Welcome to your Vega Vault,</p>
-					<p>{user.username}</p>
-				</Col>
 				<div className="text-center">
 					<p>Welcome to your Vega Vault, {user.username}</p>
 				</div>
@@ -54,7 +111,7 @@ const Vault = (props) => {
 						<tr>
 							<td>Name</td>
 							<td>Created</td>
-							<td>Data</td>
+							<td>Secret</td>
 							<td></td>
 							<td></td>
 							<td></td>
@@ -67,7 +124,7 @@ const Vault = (props) => {
 			</Row>
 			<Row>
 				<div className="text-center">
-					{addButton}
+					{addButton()}
 				</div>
 			</Row>
 		</SimplePageLayout>
